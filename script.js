264c77e63204b07ff6bad0d4299fcbc6a7930678
jonathandesb <jonathandsb20@gmail.com>
2022-11-14 11:33:03 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const btnmobile = document.getElementById('btn-mobile'); // selecionando o id btn-mobile
+const btnmobile = document.getElementById('btn-mobile'); // selecionando o id btn-mobile que esta no html
 
 
-function clicouNoMenu() { // função 
+function clicouNoMenu() { // função '' clicouNoMenu''
   const nav = document.getElementById('nav'); // selecionando o id nav
   nav.classList.toggle('active') // Adicione a class 'active' caso não tenha esta função, caso tenha remova a class 'active' 
 }
